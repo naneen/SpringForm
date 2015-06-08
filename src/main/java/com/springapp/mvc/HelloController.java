79f69fbadc2cff7d260ec42019640ba39548bb1e
naneen <neen_tta@hotmail.com>
2015-06-08 10:37:21 +0700

    could show information without check validate

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,16 +1,36 @@
 package com.springapp.mvc;
 
+import models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello");
-		return "hello";
-	}
+    @RequestMapping(value="/")
+    public String printWelcome(@ModelAttribute(value = "user")User user, ModelMap model) {
+        model.addAttribute("name", user.getName());
+        model.addAttribute("age", user.getAge());
+        model.addAttribute("address", user.getAddress());
+        return "index";
+    }
+
+//    @RequestMapping(value="/user", method=RequestMethod.POST)
+//    public createUser(Model model, @Valid @ModelAttribute("user") User user, BindingResult result){
+//        if (result.hasErrors()){
+//            // do something
+//        }
+//        else {
+//            // do something else
+//        }
+//    }
 }
